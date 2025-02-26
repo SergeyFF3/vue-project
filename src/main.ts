d52a4f67ceff42e592c9ef9420bbee81e5d0c8ea
SergeyFF3 <specialforce64044@gmail.com>
2025-02-26 12:14:17 +0300

    added store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./main.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
 import App from "./App.vue";
 
 const app = createApp(App);
---
+++ src/stores/accounts.ts
@@ -0,0 +1,69 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+interface IMark {
+  text: string;
+}
+
+enum AccountType {
+  LOCAL = "Локальная",
+  LDAP = "LDAP",
+}
+
+type AccountFieldKey = "login" | "password";
+
+interface IAccount {
+  id: string;
+  mark: IMark[];
+  type: AccountType;
+  login: string;
+  password: null | string;
+}
+
+export const useAccountStore = defineStore("account", () => {
+  const accounts = ref<IAccount[]>([]);
+
+  function addNewAccount() {
+    const id = crypto.randomUUID();
+    const newAccount: IAccount = {
+      id,
+      mark: [],
+      type: AccountType.LOCAL,
+      login: "",
+      password: "",
+    };
+
+    return [...accounts.value, newAccount];
+  }
+
+  function onChangeField(
+    id: string,
+    nameField: AccountFieldKey,
+    value: string
+  ) {
+    const account = accounts.value.find((account) => account.id === id);
+
+    if (account) {
+      account[nameField] = value;
+    }
+  }
+
+  function saveAccount(state: IAccount) {
+    return accounts.value.map((account) => {
+      if (account.id === state.id) {
+        if (state.type === AccountType.LDAP) {
+          return { ...state, password: null };
+        }
+        return state;
+      }
+
+      return account;
+    });
+  }
+
+  function deleteAccount(id: string) {
+    return accounts.value.filter((account) => account.id !== id);
+  }
+
+  return { accounts, addNewAccount, deleteAccount, saveAccount, onChangeField };
+});
