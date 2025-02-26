e26ccc84507eef0254946fb61c4b5de0d07ca5b9
SergeyFF3 <specialforce64044@gmail.com>
2025-02-26 16:51:29 +0300

    added Form component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import "./assets/App.css";
 import AddNewAccount from "./components/AddNewAccount/AddNewAccount.vue";
 import Alert from "./components/Alert/Alert.vue";
+import Form from "./components/Form/Form.vue";
 </script>
 
 <template>
   <div class="app__content">
     <AddNewAccount />
     <Alert />
+    <Form />
   </div>
 </template>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useAccountStore } from "../../stores/accounts";
+
+const accountStore = useAccountStore();
+const accounts = computed(() => accountStore.accounts);
+</script>
+
+<template>
+  <el-table :data="accounts" style="width: 100%">
+    <el-table-column prop="mark" label="Метки" min-width="22%" />
+    <el-table-column prop="type" label="Тип записи" min-width="14%" />
+    <el-table-column prop="login" label="Логин" min-width="22%" />
+    <el-table-column prop="password" label="Пароль" min-width="22%" />
+    <el-table-column min-width="5%"> </el-table-column>
+  </el-table>
+</template>
